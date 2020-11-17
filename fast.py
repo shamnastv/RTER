e6a2bc1e88dade500134e9f90a00274204bc321d
Shamnas <shamnastv@gmail.com>
2020-11-17 11:15:52 +0530

    update

--- fast.py
+++ fast.py
@@ -4,6 +4,7 @@ dataset = 'data/fil9'
 # model = fasttext.train_unsupervised(dataset)
 model = fasttext.train_unsupervised('data/fil9', minn=2, maxn=5, dim=300)
 
+print(model.get_word_vector("enviroment"))
 model.save_model("/scratch/shamnast/model")
 
 print('model saved')
