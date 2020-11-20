13ba35cf5b9274b80576981d5c241a4c567aebab
Shamnas <shamnastv@gmail.com>
2020-11-20 11:51:00 +0530

    update

--- run.sh
+++ run.sh
@@ -6,13 +6,12 @@
 #SBATCH --gres=gpu:1
 #SBATCH --partition=cl1_48h-1G
 
-python3 main.py --hidden_dim 300 --max_window_size 40 --epochs 50 --lr 2.5e-4 --dataset MELD --dropout .5 --num_layers 1
-python3 main.py --hidden_dim 300 --max_window_size 40 --epochs 50 --lr 1e-4 --dataset MELD --dropout .5 --num_layers 1
+python3 main.py --hidden_dim 300 --max_window_size 40 --epochs 50 --lr 5e-5 --dataset MELD --dropout .5 --num_layers 1
+python3 main.py --hidden_dim 300 --max_window_size 40 --epochs 50 --lr 8e-5 --dataset MELD --dropout .5 --num_layers 1
 
 #python3 main.py --hidden_dim 300 --max_window_size 40 --epochs 100 --lr 2.5e-4 --dataset MELD --dropout .3 --num_layers 1
 #python3 main.py --hidden_dim 300 --max_window_size 40 --epochs 100 --lr 2.5e-4 --dataset MELD --dropout .5 --num_layers 1
 #python3 main.py --hidden_dim 300 --max_window_size 40 --epochs 100 --lr 2.5e-4 --dataset MELD --dropout .2 --num_layers 1
-
 
 
 #python3 main.py --hidden_dim 100 --max_window_size 30 --epochs 100 --lr 1e-3 --dataset MELD --dropout .3 --num_layers 1
